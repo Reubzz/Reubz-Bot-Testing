658f56e54fd4bf6379a8ff51fa72b62cdbeb1e1f
Reubz <reugeorac@gmail.com>
2021-10-15 16:05:03 +0530

    Upgraded Ready Console Log with more info about bot.

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,34 @@
 const client = require("../index");
+const config = require('../config.json');
 var colors = require('colors/safe')
 
-client.on("ready", () =>
-    console.log(colors.brightGreen.bold(`${client.user.tag} is up and ready to go!`))
-);
+
+// client.on("ready", () =>
+//     console.log(colors.brightGreen.bold(`${client.user.tag} is up and ready to go!`))
+// );
+
+client.on("ready", async () => {
+  console.log(colors.green.bold("Success!"))
+  console.log(colors.gray("Connected To"), colors.yellow(`${client.user.tag}`));
+  console.log(
+    colors.white("Watching"),
+    colors.red(`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`),
+    colors.white(`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0) > 1 ? "Users," : "User,"}`),
+    colors.red(`${client.guilds.cache.size}`),
+    colors.white(`${client.guilds.cache.size > 1 ? "Servers." : "Server."}`)
+  )
+  console.log(
+    colors.white(`Prefix:` + colors.red(` ${config.prefix}`)),
+    colors.white("||"),
+    colors.red(`${client.commands.size}` + ` Commands`),
+    colors.white("||"),
+    colors.red(`${client.slashCommands.size}` + ` Slash Commands`),
+  );
+  console.log("")
+  console.log(colors.red.bold("——————————[Statistics]——————————"))
+  console.log(colors.gray(`Running on Node ${process.version} on ${process.platform} ${process.arch}`))
+  console.log(colors.gray(`Memory: ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS\n${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`))
+})
 
 
 /*
