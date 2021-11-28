c30c5bd1c6f78ac117978e9bc4392a35adbf5780
Reubz <reugeorac@gmail.com>
2021-11-28 16:46:28 +0530

    games added

--- commands/info/miscellaneous/tictactoe.js
+++ commands/info/miscellaneous/tictactoe.js
@@ -1,9 +1,16 @@
 const simplyjs = require("simply-djs");
+const { Message, Client, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports ={
     name: "tictactoe",
     aliases: ["x&o", "xo", "ox"],
     description: "A interactive Tic Tac Toe Command.",
+    
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
 
     run: async (client, message, args) => {
         simplyjs.tictactoe(message, {
