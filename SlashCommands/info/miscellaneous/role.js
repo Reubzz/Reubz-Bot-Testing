368ce9fea9e7c6a8c63a11184e7f2b58dce75574
Reubz <reugeorac@gmail.com>
2021-11-28 15:17:40 +0530

    Added Check for Role if not create new role.

--- SlashCommands/info/miscellaneous/role.js
+++ SlashCommands/info/miscellaneous/role.js
@@ -1,4 +1,5 @@
-const { Client, CommandInteraction, MessageActionRow, MessageButton } = require("discord.js");
+const { Client, CommandInteraction, MessageActionRow, MessageButton, Permissions } = require("discord.js");
+const color = require("../../../commands/info/Reaction Roles/color");
 
 module.exports = {
     name: "verify",
@@ -11,6 +12,26 @@ module.exports = {
      * @param {String[]} args
      */
     run: async (client, interaction, args) => {
+
+        const checkVerifiedRole = interaction.guild.roles.cache.find((x) => x.name === 'Test Role')
+        const checkRolePerms = interaction.guild.me.permissions.has(Permissions.FLAGS.MANAGE_ROLES) // permissionsFor(client).has(Permissions.FLAGS.MANAGE_ROLES);
+
+        if(!checkVerifiedRole && !checkRolePerms){
+            return interaction.followUp({ content: 'I can\'t find a Role Named **Test**, Missing Perms `MANAGE_ROLES` hence couldn\'t create a New Test Role.', ephemeral: 1 })
+        }
+        else if(!checkVerifiedRole && checkRolePerms){
+            await interaction.guild.roles.create({ name: 'Test Role', 
+                                                   permissions: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.VIEW_CHANNEL],
+                                                   color: 'BLURPLE',
+                                                   hoist: 1,
+                                                   mentionable: 1,
+                                                   reason: 'Created a Verify Role as the Server didn\'t have one.',
+                                                })                       
+            const verifiedRole = interaction.guild.roles.cache.find((role) => role.name === 'Test Role')
+            console.log('New Test Role Created.')
+            interaction.followUp({ content: `I could not find a **Test** Role here, So I Created it - ${verifiedRole}. Create Permision Overides for this Role in your server.`})
+        }
+        
         
         const embed1 = {
             
