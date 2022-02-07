6524fa8158bb4cecf23f50a9178d2d4d559949b9
Reubz <reugeorac@gmail.com>
2022-02-08 00:16:38 +0530

    Added ability for Required Roles.

--- SlashCommands/info/Leveling System/setCustomRankCard.js
+++ SlashCommands/info/Leveling System/setCustomRankCard.js
@@ -1,5 +1,6 @@
 const { Client, CommandInteraction, MessageActionRow, MessageButton } = require('discord.js');
-const rankCardDB = require("../../../models/CustomRankCard")
+const rankCardDB = require("../../../models/databases/CustomRankCard");
+const reqRolesDB = require("../../../models/databases/reqRoles");
 
 module.exports = {
     name: 'custom-rank-card',
@@ -43,11 +44,62 @@ module.exports = {
      */
 
     run: async (client, interaction, args) => {
+        const commandUser = interaction.guild.members.cache.get(interaction.user.id);
+
         const imgURL = interaction.options.getString('url');
         const levelbar = interaction.options.getString('level-bar');
-        
         const defaultValues = interaction.options.getString('set-default-values')
 
+        // const u = new reqRolesDB({
+        //     gid: interaction.guild.id,
+        //     commandName: "setCustomRankCard"
+        // })
+        // await u
+        //     .save()
+        //     .catch((e) => { interaction.followUp({ content: e }) });
+
+        let checkRoles = 0;
+        let allowedRoles = [];
+        console.log("initial value "+checkRoles) // debug
+
+        const checkReqRolesDB = await reqRolesDB.findOne({ gid: interaction.guild.id, commandName: "setCustomRankCard" })
+        if(!checkReqRolesDB){
+            checkRoles++
+            checkRoles++
+            console.log("if no db found checkRoles value "+checkRoles) // debug
+        }
+
+        else if(checkReqRolesDB){
+            allowedRoles = checkReqRolesDB.reqRoles
+            console.log(checkReqRolesDB.reqRoles) // debug
+            if(allowedRoles.length == 0){
+                await reqRolesDB.deleteMany({ gid: interaction.guild.id, commandName: "setCustomRankCard" })
+                checkRoles++
+                checkRoles++
+                console.log("if no reqRoles found in db value "+checkRoles) // debug
+            }
+            console.log("IMP = if reqRoles are found in db value "+checkRoles) // debug
+        }
+        
+        console.log("Final checkRoles value "+checkRoles) // debug
+
+        if(checkRoles == 0){
+            await allowedRoles.forEach((role) => { 
+                if(commandUser.roles.cache.has(role)) {
+                    checkRoles++
+                };
+            })
+        }
+
+        if(checkRoles == 0) {
+            return interaction.followUp({ 
+                content: 'You cannot use this command. You need to have atleast 1 of the following Roles to use this command!\n' +
+                        `<@&${allowedRoles.toString().replaceAll(',', `>\n<@&`)}>`,
+                allowedMentions: { parse: ["users"] }
+            })
+        }
+
+        
         const checkMember = await rankCardDB.findOne({ user: interaction.user.id, gid: interaction.guild.id })
 
         // Creating Default Entry in DB.
