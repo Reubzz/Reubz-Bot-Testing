c0cdad482a871c350b55b0fd8b395e5be221ae7a
Reubz <reugeorac@gmail.com>
2022-03-12 22:49:03 +0530

    Locked the Command to Owner Only!!

--- SlashCommands/info/Admin/restart.js
+++ SlashCommands/info/Admin/restart.js
@@ -1,5 +1,5 @@
 const { Client, CommandInteraction, MessageActionRow, MessageButton, Message } = require('discord.js');
-const { watchFile } = require('fs');
+const { owner } = require("../../../config.json");
 require('dotenv').config();
 
 module.exports = {
@@ -14,13 +14,17 @@ module.exports = {
      */
 
     run: async (client, interaction, args) => {
-        await interaction.followUp({ content: "Restarting Bot ..."})
+        if(owner.includes(interaction.user.id)){
 
-        // Simple Restart command that throws a error to the console. 
-        // And the BAT script which is a Loop that restarts bot whenever it crashes.
-        // Using both we have made a simple restart command. 
-        setTimeout(() => {
-            throw new Error("Restarting Bot!!")
-        }, 1000 * 2)
+            await interaction.followUp({ content: "Restarting Bot ..."})
+    
+            // Simple Restart command that throws a error to the console. 
+            // And the BAT script which is a Loop that restarts bot whenever it crashes.
+            // Using both we have made a simple restart command. 
+            setTimeout(() => {
+                throw new Error("Restarting Bot!!")
+            }, 1000 * 2)
+        }
+        else interaction.followUp({ content: "Sorry this Command is strictly **`OWNER ONLY`**"})
     }
 };
